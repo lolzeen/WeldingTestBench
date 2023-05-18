cd6a076470c4e37ba364ad58b4d385cfbda1b4de
lolzeen <leonardo_nogueira13@hotmail.com>
2023-05-17 23:19:29 -0300

    feat: instantiated all constant variables (arduino pins and process parameters), defined the arc controller interrupt capture function and elaborated main file structure

---
+++ include/main.hpp
@@ -0,0 +1,66 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Arduino.h>
+#include "DisplayController.hpp"
+#include "MemoryController.hpp"
+#include "ProcessController.hpp"
+#include "MotorController.hpp"
+#include "Motor.hpp"
+#include "EndSwitchController.hpp"
+#include "EndSwitch.hpp"
+#include "DriverPins.hpp"
+#include "DisplayPins.hpp"
+#include "InputPins.hpp"
+
+/* ----- Driver Pins ----- */
+// azul = EN, verde = DIR, amarelo = STEP
+const int ENX = 23;
+const int DIRX = 25;
+const int STEPX = 5;
+const int HORIZONTAL_ENDSWITCH_START = 20; 
+const int HORIZONTAL_ENDSWITCH_END = 21;
+const int OUTX = 33;
+const int ENZ = 29;
+const int DIRZ = 27;
+const int STEPZ = 6;
+const int VERTICAL_ENDSWITCH_START = 19;
+const int VERTICAL_ENDSWITCH_END = 18;
+const int OUTZ = 35;
+DriverPins driver_x_pins = {DIRX, ENX, STEPX, OUTX};
+DriverPins driver_z_pins = {DIRZ, ENZ, STEPZ, OUTZ};
+
+/* ----- Display Pins ----- */
+const uint8_t display_rs = 13;
+const uint8_t display_en = 12;
+const uint8_t display_d4 = 11;
+const uint8_t display_d5 = 10;
+const uint8_t display_d6 = 9;
+const uint8_t display_d7 = 8;
+DisplayPins display_pins = {display_rs, display_en, display_d4, display_d5, display_d6, display_d7};
+
+/* ---- Input Pins ----- */
+const uint8_t button = 31;
+const uint8_t encoder_a = 3; // 3;
+const uint8_t encoder_b = 2; // 25;
+InputPins input_pins = {button, encoder_a, encoder_b};
+
+/* ---- Driver Parameters ----- */
+const uint16_t driver_params = 8000; // 8000 pulses per revolution
+const uint8_t EIXO_X_ID = 3;
+const uint8_t EIXO_Z_ID = 4; 
+const uint8_t EIXO_X_DIS = 34; // cm
+const uint8_t EIXO_Z_DIS = 33; // cm
+const uint8_t CORRECT_HEIGHT = 1;
+
+/* ---- MotorParameters  ----- */
+const uint8_t ROLLBACK_DELAY = 200 // ms
+// const uint8_t 
+
+/* ---- DisplayParameters  ----- */
+const uint8_t DISPLAY_REFRESH_RATE = 100 // ms
+const uint8_t DISPLAY_REFRESH_RATE_DURING_PROCESS = 1000 // ms
+
+/* ----   ----- */
+
+#endif // MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
-#include <Arduino.h>
+#include "main.hpp"
+
+ISR(TIMER5_COMPA_vect) {
+  ProcessController::readArcVoltage(); // arc voltage variable should be declared as volatile and static
+  //TODO:  enable arc voltage reading
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  MemoryController memoryController = &MemoryController::getInstance();
+  memoryController.updateWeldingParametersFromEeprom();
+
+  MotorController motorController = &MotorController::getInstance():
+  motorController.setHorizontalAxisMotor(Motor(driver_x_pins, driver_params, EIXO_X_ID, EIXO_X_DIS, true));
+  motorController.setVerticalAxismotor(Motor(driver_z_pins, driver_params, EIXO_Z_ID, EIXO_Z_DIS, true));
+
+  EndSwitchController endSwitchController = &EndSwitchController::getInstance();
+  endSwitchController.setHorizontalAxisStartPin(HORIZONTAL_ENDSWITCH_START);
+  endSwitchController.setHorizontalAxisEndPin(HORIZONTAL_ENDSWITCH_END);
+  endSwitchController.setVerticalAxisStartPin(VERTICAL_ENDSWITCH_START);
+  endSwitchController.setVerticalAxisEndPin(VERTICAL_ENDSWITCH_END);
+
+  ProcessController processController = &ProcessController::getInstance();
+  processController.firstTimeSetup();
+
+  DisplayController displayController = &DisplayController::getInstance();
+  displayController.setRefreshRate(DISPLAY_REFRESH_RATE, DISPLAY_REFRESH_RATE_DURING_PROCESS);
+  displayController.firstTimeSetup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  MemoryController memoryController = &MemoryController::getInstance();
+  ProcessController processController = &ProcessController::getInstance();
+  DisplayController displayController = &DisplayController::getInstance();
+  processController.monitorProcess(); // TODO: check process parameters: endswitches and arc length control
+  // ProcessController.run();
+  // TODO: process user input
+  displayController.updateDisplay(); // TODO: update display every XX ms
 }
+
